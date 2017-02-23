5bf80b60a7a065e5d23739c9413d86b56b8a8aa4
StanislavVoronov <stanislavvoronov@bk.ru>
2017-02-23 12:13:09 +0300

    Add files via upload

--- components/actions/index.js
+++ components/actions/index.js
@@ -1,1 +1,94 @@
+export const UPDATE_DOC_TYPES_LIST="UPDATE_DOC_TYPES_LIST"
+export const UPDATE_FILE_LIST="UPDATE_FILE_LIST"
+export const UPDATE_UPLOAD_IMAGE_FILE="UPDATE_UPLOAD_IMAGE_FILE"
+export const UPDATE_FILE_ITEM_TYPE="UPDATE_FILE_ITEM_TYPE"
+export const UPDATE_FILE_ITEM_STATUS="UPDATE_FILE_ITEM_STATUS"
+export const DELETE_FILE_ITEM="DELETE_FILE_ITEM"
+export const UPDATE_DOC_NEED_SCANS="UPDATE_DOC_NEED_SCANS"
+export const ADD_NEW_FILE_TO_SERVER="ADD_NEW_FILE_TO_SERVER"
+export const GET_USER_WORK_ROOM="GET_USER_WORK_ROOM"
+export const SET_REQUEST_PERFORMER="SET_REQUEST_PERFORMER"
+export const SET_PRIEM_USER="SET_PRIEM_USER"
+export const SET_LOGGED_PRIEM_USER="SET_LOGGED_PRIEM_USER"
+export const SET_NEW_TYPE_REQUEST="SET_NEW_TYPE_REQUEST"
+export const ADD_NEW_PRIEM_REQUEST="ADD_NEW_PRIEM_REQUEST"
+export const CHECK_BEFORE_SEND_REQUEST="CHECK_BEFORE_SEND_REQUEST"
+export const GET_DOWNLOAD_FILE_IMAGE="GET_DOWNLOAD_FILE_IMAGE"
+export const SYSTEM_STATUS_STATE="SYSTEM_STATUS_STATE"
+/////////////////////////////////////////////
+import reduxSagaActions from './sagaActions'
+export default function* PriemRootSaga(state)
+{
+ 	yield [...reduxSagaActions]
+}
+/////////////////////////////////////////////
+
+
+
+
+export const updateDocTypeList=(items)=>
+{
+	return {type:UPDATE_DOC_TYPES_LIST,items}
+}
+export const addNewFileToServer=(item)=>
+{
+	return {type:ADD_NEW_FILE_TO_SERVER,item}
+}
+export const getUserWorkRoom=(items)=>
+{
+	return {type:GET_USER_WORK_ROOM,items}
+}
+
+export const updateDocNeedScans=(items)=>
+{
+	return {type:UPDATE_DOC_NEED_SCANS,items}
+}
+
+export const updateFileList=(items)=>
+{
+	return {type:UPDATE_FILE_LIST,items}
+}
+
+export const updateFileItemType=(item,typeDoc)=>
+{
+	return {type:UPDATE_FILE_ITEM_TYPE,
+		item,
+		typeDoc
+	}
+}
+
+export const updateFileItemStatus=(item,status)=>
+{
+
+	return {type:UPDATE_FILE_ITEM_STATUS,item,status}
+}
+
+export const deleteImageFile=(item)=>
+{
+	return {type:DELETE_FILE_ITEM,item}
+}
+export const setRequestPerformer=(item)=>
+{
+	return {type:SET_REQUEST_PERFORMER,item}
+}
+export const setPriemUser=(item)=>
+{
+	return {type:SET_PRIEM_USER,item}
+}
+export const setLoggedUser=(item)=>
+{
+	return {type:SET_LOGGED_PRIEM_USER,item}
+}
+export const sendNewPriemRequestForServer=(item)=>
+{
+	return {type:ADD_NEW_PRIEM_REQUEST,item}
+}
+export const setNewTypeRequest=(item)=>
+{
+	return {type:SET_NEW_TYPE_REQUEST,item}
+}
+export const getUserUploadImage=()=>
+{
+	return {type:GET_DOWNLOAD_FILE_IMAGE}
+}
 
---
+++ components/actions/sagaActions.js
@@ -0,0 +1,230 @@
+
+import {UPDATE_DOC_TYPES_LIST,
+    UPDATE_FILE_TYPE,
+    UPDATE_DOC_NEED_SCANS,
+    ADD_NEW_FILE_TO_SERVER,
+    UPDATE_UPLOAD_IMAGE_FILE,
+    GET_USER_WORK_ROOM,
+    SET_PRIEM_USER,
+    DELETE_FILE_ITEM,
+    SET_LOGGED_PRIEM_USER,
+    ADD_NEW_PRIEM_REQUEST,
+    GET_DOWNLOAD_FILE_IMAGE,
+    UPDATE_FILE_LIST} from './'
+import {put,call,takeEvery,take,takeLatest,fork,select} from 'redux-saga/effects'
+import {networkService} from "../common/networkService";
+export function* getDownloadFileImage()
+{
+	yield take(GET_DOWNLOAD_FILE_IMAGE)
+	const userData = JSON.parse(localStorage["priemUser"])
+	let item;
+	try 
+    {
+			items = yield call(networkService,
+			{name:"userFileImage",data:userData.id_operator,method:"GET"})
+			yield put({type:UPDATE_FILE_LIST,items})
+	}catch(error)
+	{
+		yield put({type:UPDATE_FILE_LIST,item})
+	}
+
+}
+export function* setLoggedPriemUser()
+{
+		yield take(SET_LOGGED_PRIEM_USER)
+		const userData = JSON.parse(localStorage["priemUser"])
+		let item;
+  	  	try 
+	    {
+				item = yield call(networkService,
+				{name:"connOper",data:userData,method:"POST"})
+				yield put({type:SET_LOGGED_PRIEM_USER,item})
+		}catch(error)
+		{
+			yield put({type:SET_LOGGED_PRIEM_USER,item})
+		}
+}
+export function* setLoginUser(state)
+{
+		yield take(SET_PRIEM_USER)
+		const userData = yield select(state => state.PriemAccount.user)
+		console.log(userData)
+		let item;
+  	  	try 
+	    {
+				item = yield call(networkService,
+				{name:"regOper",data:userData,method:"POST"})
+				const priemUser=JSON.stringify(item)
+				if (priemUser.id_operator==0) {
+					alert("Error has occured")
+					item=undefined
+				}
+				else 
+				{
+					localStorage["priemUser"]=JSON.stringify(item)
+				}
+				
+				yield put({type:SET_PRIEM_USER,item})
+		}catch(error)
+		{
+			yield put({type:UPDATE_DOC_NEED_SCANS,items})
+		}
+
+
+}
+export function* getDocNeedScans(state)
+{
+		
+		yield take(UPDATE_DOC_NEED_SCANS)
+		let items;
+  	  	try 
+	    {
+	    		
+				items = yield call(networkService,
+				{name:'RequestTypesNeedScans'})
+				yield put({type:UPDATE_DOC_NEED_SCANS,items})
+		}catch(error)
+		{
+			yield put({type:UPDATE_DOC_NEED_SCANS,items})
+		}
+		  
+}
+export function* sendNewFileToServer()
+{
+		while(true) {
+		    yield take(ADD_NEW_FILE_TO_SERVER)
+			const file = yield select(state => {
+				return state.PriemAddNewRequest.DocFileList.find((file,index)=> 
+					state.PriemAddNewRequest.needsUpload==index)
+			});
+		  	let image;
+		    try 
+		    {
+				 image = yield call(networkService,
+						 {	
+							name:file.name,
+							data:file,
+							format:true,
+							url:'dev-bin/priem_image_upload?json=1',
+							method:'POST'})
+				yield put({type:UPDATE_UPLOAD_IMAGE_FILE,item:image})
+			}catch(error)
+			{
+				console.log(error)
+			}
+		  }
+		
+		
+}
+export function* removeFileImageFromServer()
+{
+		while(true) {
+		    yield take(DELETE_FILE_ITEM)
+			const imageId = yield select(state => state.PriemAddNewRequest.needsImageRemove);
+		    try 
+		    {
+				 yield call(networkService,
+						 {	
+							name:"removeUploadImageFile",
+							data:imageId,
+							format:true,
+							method:'POST'})
+			}catch(error)
+			{
+				console.log(error)
+			}
+		  }
+		
+		
+}
+export function* getRequestUserWorkRoom()
+{
+   		
+		yield take(GET_USER_WORK_ROOM)
+		let items;
+	    try 
+	    {
+			items = yield call(networkService,{name:'userWorkRoom'})
+			yield put({type:GET_USER_WORK_ROOM,items})
+		}catch(error)
+		{
+			yield put({type:GET_USER_WORK_ROOM,items:items})
+		}
+  
+}
+export function* getDocList()
+{
+   		
+		yield take(UPDATE_DOC_TYPES_LIST)
+		let items;
+	    try 
+	    {
+			items = yield call(networkService,{name:'DocumentList'})
+			yield put({type:UPDATE_DOC_TYPES_LIST,items})
+		}catch(error)
+		{
+			yield put({type:UPDATE_DOC_TYPES_LIST,items:items})
+		}
+  
+}
+
+export function* addNewPriemRequest()
+{
+   		
+		yield take(ADD_NEW_PRIEM_REQUEST)
+		let items;
+	   	const files = yield select(state => {
+				return state.PriemAddNewRequest.DocFileList.filter(file=> 
+				file.selected==true)
+		}); 
+		const operator=yield select(state => {
+				return state.PriemAddNewRequest.requestPerformer ? state.PriemAddNewRequest.requestPerformer.id_oper : state.PriemAccount.user.id
+		});
+		const typeRequest=yield select(state => {
+				return state.PriemAddNewRequest.typeRequest
+		});
+		let requestData= new FormData(); 
+		let error=false
+		const typesDocFile=files.map(file=>{
+			requestData.append(file.name,file)
+			if (file.typeDoc.typeDoc || file.typeDoc.subTypeDoc)
+			{
+				return Object.assign({},{typeDoc:file.typeDoc.typeDoc},{subTypeDoc:file.typeDoc.subTypeDoc})
+			}
+			else 
+			{
+				error=true;
+				alert('У документа '+file.name+' отсутствует тип документа. Для добавления документа в пакет необходимо выбрать тип документа, либо исключить документ из пакета')
+			}
+		})
+		if (error)
+		{
+			put({type:ADD_NEW_PRIEM_REQUEST,item:'error'})
+			return false
+		}
+		requestData.append('addNewTypeRequest',typeRequest.id);
+		requestData.append('operatorIdNewRequest',operator);
+  		requestData.append('newDocFilesRequest',JSON.stringify(typesDocFile));
+  		console.log("requestData",requestData)
+  		const zaprosNumber=yield call(networkService,
+							{	
+								name:"addNewRequest",
+								formData:requestData,
+								format:true,
+								method:'POST'
+							})
+  		console.log("zaprosNumber",zaprosNumber)
+  		put({type:ADD_NEW_PRIEM_REQUEST,item:zaprosNumber})
+}
+const reduxSagaActions=[
+addNewPriemRequest(),
+getDocList(),
+getRequestUserWorkRoom(),
+removeFileImageFromServer(),
+sendNewFileToServer(),
+getDocNeedScans(),
+setLoginUser(),
+setLoggedPriemUser(),
+getDownloadFileImage(),
+]
+export default reduxSagaActions
